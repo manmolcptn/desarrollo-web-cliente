13780549902109aca36181cda5db0147f39f86be
manmolcptn <molinacapitanmiguelangel@gmail.com>
2024-11-04 14:44:59 +0100

    Arreglo ejercicio9

--- EjerciciosDom/Ejercicio9/script.js
+++ EjerciciosDom/Ejercicio9/script.js
@@ -1,23 +1,25 @@
-window.onload = () =>{
+window.onload = function(){
     changeStyles();
-	// apartadoA();
+	//apartadoA();
 }
 
 function changeStyles(){
 	let p1 = document.getElementsByTagName("p")[0];
+	let p2 = document.getElementsByTagName("p")[1];
+
 	p1.onclick = function(){
-		this.sytle.backgroundColor = "yellow";
+		this.style.backgroundColor = "yellow";
 		this.style.border="1px solid black";
 		this.style.borderRadius= "15px";
 		this.style.padding="10px";
 	}
 
-	// p2.onclick = function(){
-	// 	this.sytle.backgroundColor = "red";
-	// 	this.style.border="2px solid black";
-	// 	this.style.borderRadius= "5px";
-	// 	this.style.padding="10px";
-	// }
+	p2.onclick = function(){
+		this.style.backgroundColor = "red";
+		this.style.border="2px solid black";
+		this.style.borderRadius= "5px";
+		this.style.padding="10px";
+	}
 
 }
 function apartadoA(){
